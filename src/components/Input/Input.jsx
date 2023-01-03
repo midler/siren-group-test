0baeda70e00d2db44feb3096b80419d801fe66dc
Andrey Seredkin <seredkin@htmlacademy.ru>
2023-01-04 02:46:54 +0400

    Add Input Component

---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import clsx from "clsx";
+
+import "./Input.scss";
+
+export const Input = ({ label, type, value, onChange, placeholder, error }) => {
+  const [focus, useFocus] = useState(false);
+
+  const useFocusOnHandler = () => {
+    useFocus(true);
+  };
+
+  const useFocusOffHandler = () => {
+    useFocus(false);
+  };
+
+
+  return (
+    <div className="input">
+      <label
+        className={clsx(
+          "input__wrapper",
+          value ? "input__wrapper--not-empty" : "input__wrapper--empty",
+          focus && 'input__wrapper--focus'
+        )}
+      >
+        <span className="input__label">{label}</span>
+        <input
+          className="input__field"
+          type={type}
+          value={value}
+          onChange={onChange}
+          placeholder={placeholder}
+          onFocus={useFocusOnHandler}
+          onBlur={useFocusOffHandler}
+        />
+      </label>
+
+      {error && <div className="input__error">{error}</div>}
+    </div>
+  );
+};
+
+Input.defaultProps = {
+  type: "text",
+  value: "",
+  onChange: () => {},
+  placeholder: "",
+  error: "",
+};
+
+Input.propTypes = {
+  label: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+  placeholder: PropTypes.string,
+  error: PropTypes.string,
+};
